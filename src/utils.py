bc7660cd0d6e0da19079841680f779e1fa12ea4a
KeeganThompson <keegantcollege@gmail.com>
2025-09-19 18:29:56 -0400

    Hyperparam tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path: str, obj) -> None:
 	"""Serialize `obj` to `file_path` using pickle.
@@ -21,7 +22,7 @@ def save_object(file_path: str, obj) -> None:
 	except Exception as e:
 		raise CustomException(e, sys)
 	
-def evaluate_model(X_train, y_train, X_test, y_test, models: dict) -> dict:
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     """Train and evaluate multiple models.
 
     Trains each model in `models` on the training data and evaluates it on the test data.
@@ -31,11 +32,23 @@ def evaluate_model(X_train, y_train, X_test, y_test, models: dict) -> dict:
     try:
         report = {}
 
-        for model_name, model in models.items():
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
-            report[model_name] = test_model_score
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
     except Exception as e:
