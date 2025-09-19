24f245751cac6cd71ee0e421b5525f578784d0c7
KeeganThompson <keegantcollege@gmail.com>
2025-09-19 17:09:53 -0400

    data

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from setuptools import find_packages, setup
+from typing import List
 
 
+E_DOT="-e ."
+def get_requirements(file_path:str)->List[str]:
+    '''This function will return the list of requirements'''
+    requirements=[]
+    with open('requirements.txt') as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if E_DOT in requirements:
+            requirements.remove(E_DOT)
+    return requirements
 
 setup(
     name="MLpersonalProject",
@@ -8,5 +20,5 @@ setup(
     author="Keegan Thompson",
     author_email='keeganwthom@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas', 'numpy', 'seaborn']
+    install_requires=get_requirements('requirements.txt')
 )
