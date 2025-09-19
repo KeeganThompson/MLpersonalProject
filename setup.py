b6addabc0625e3bf25e1af933ffde48dd9b58ec0
KeeganThompson <keegantcollege@gmail.com>
2025-09-19 15:27:05 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name="MLpersonalProject",
+    version='0.0.1',
+    author="Keegan Thompson",
+    author_email='keeganwthom@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'seaborn']
+)
